db289da0f882dfc0e7be24c6df98ba69c35f8410
Navneet <navn0306@gmail.com>
2021-11-23 23:15:25 +0530

    Hello World

---
+++ src/main/java/com/bridge/demo/test.java
@@ -0,0 +1,18 @@
+package com.bridge.demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class test {
+    public static final Logger LOG = LogManager.getLogger(test.class);
+    public static void main(String[] args) {
+        String message = "Welcome";
+        System.out.println("Hello using maven");
+        LOG.debug(message + "will be printer on Debug");
+        LOG.info(message + "will be printer on info");
+        LOG.warn(message + "will be printer on warn");
+        LOG.error(message + "will be printer on error");
+        LOG.fatal(message + "will be printer on fatal");
+        LOG.debug("Appending String: {}.", message);
+        System.out.println(message);
+    }
+}
